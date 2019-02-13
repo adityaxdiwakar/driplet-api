90f0d5662b4fe8c895a4ee6ab6570a2e92b891a9
Aditya <aditya@diwakar.io>
2019-02-13 09:06:00 -0500

    Added services manager to create, delete, and get services using flatfiles
    Files Changed:
    - app.py
    - services.py
    
    Next Steps:
    - Crucially needs a registration endpoint to create your client_id and flatfile directory
    
    !!! Considering to move to MongoDB rather than using flatfiles

---
+++ services/services.py
@@ -0,0 +1,68 @@
+#general requirements
+import time, json, os, random
+
+#API dependencies
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+
+#helper functions
+def check_dupe(list1, value):
+    for item in list1:
+        if item['id'] == value:
+            return True
+    return False
+
+def get_service_id(client_services):
+    while True:
+        r = random.randint(1000000000,9999999999)
+        if not check_dupe(client_services, r):
+            return 's' + str(r)
+
+def get_services(client_id):
+    with open(f"bin/{client_id}/services.json", "r") as f:
+        return json.load(f)
+
+def push_services(data, client_id):
+    with open(f"bin/{client_id}/services.json", "w") as f:
+        json.dump(data, f, indent=4)
+
+class manager(Resource):
+    def get(self, client_id):
+        return get_services(client_id)
+    def post(self, client_id):
+        user_services = get_services(client_id)
+        
+        parser = reqparse.RequestParser()
+        parser.add_argument("name")
+        parser.add_argument("start_command")
+        parser.add_argument("stop_command")
+        parser.add_argument("status_command")
+        parser.add_argument("log_command")
+        args = parser.parse_args()
+        
+        service = {
+            "name": args['name'],
+            "id": get_service_id(get_services(client_id)),
+            "start_command": args['start_command'],
+            "stop_command": args['stop_command'],
+            "status_command": args['status_command'],
+            "log_command": args['log_command']
+        }
+
+        user_services.append(service)
+        push_services(user_services, client_id)
+        return service, 201
+
+class manager_indv(Resource):
+    def get(self, client_id, service_id):
+        services = get_services(client_id)
+        for service in services:
+            if service['id'] == service_id:
+                return service
+    def delete(self, client_id, service_id):
+        services = get_services(client_id)
+        for x in range(len(services)):
+            if services[x]['id'] == service_id:
+                preq = services[0:x] + services[x+1:len(services)+1]
+                push_services(preq, client_id)
+                return preq
