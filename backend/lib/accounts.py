bf8871991b3268a69bb9acb2b96fab9b65d8317f
Aditya Diwakar <i@adityadiwakar.me>
2019-03-01 18:24:47 -0500

    [fe] add option to logout on service page

--- backend/lib/accounts.py
+++ backend/lib/accounts.py
@@ -1,30 +1,38 @@
-#password hashing
+# password hashing
 from passlib.apps import custom_app_context as pwd_context
 
-#general requirements
-import time, json, os, random, shutil, string, jwt
+# general requirements
+import time
+import json
+import os
+import random
+import shutil
+import string
+import jwt
 
-#API dependencies
+# API dependencies
 from flask import Flask, request
 from flask_restful import Api, Resource, reqparse
 
-#string definitions
+# string definitions
 NOT_FOUND = {"message": "User could not be found", "code": 404}
 AUTH_FAILED = {"message": "Authorization failed", "code": 401}
-UNAME_EXISTS = {"message": "A user with that username already exists", "code": 400}
-EMAIL_EXISTS = {"message": "A user with that email already exists", "code": 400}
-TOKEN_VERIFIED ={"message": "Successfully verified your token", "code": 200}
+UNAME_EXISTS = { "message": "A user with that username already exists", "code": 400}
+EMAIL_EXISTS = { "message": "A user with that email already exists", "code": 400}
+TOKEN_VERIFIED = {"message": "Successfully verified your token", "code": 200}
 
 
-#helper functions
+# helper functions
 def random_salt(size=1024, chars=string.ascii_uppercase + string.digits + string.ascii_lowercase):
-        return ''.join(random.choice(chars) for _ in range(size))
+    return ''.join(random.choice(chars) for _ in range(size))
+
 
 def get_user_id():
     while True:
-        r = random.randint(1000000000,9999999999)
+        r = random.randint(1000000000, 9999999999)
         if r not in os.listdir('bin'):
             return 'u' + str(r)
+
 
 def get_users():
     users = []
@@ -33,6 +41,7 @@ def get_users():
         users.append(data)
     return users
 
+
 def get_user(id):
     users = os.listdir('bin')
     if str(id) not in users:
@@ -40,28 +49,32 @@ def get_user(id):
     else:
         return json.load(open(f"bin/{id}/account.json", "r"))
 
+
 def push_user(user):
     os.mkdir(f"bin/{user['id']}")
     json.dump(
         user,
         open(f"bin/{user['id']}/account.json", "w"),
-        indent = 4
+        indent=4
     )
     json.dump(
         [],
         open(f"bin/{user['id']}/services.json", "w"),
-        indent = 4
+        indent=4
     )
+
 
 def public_user(user):
     user.pop('salt')
     user.pop('password')
     return user
 
-def generate_token(user, salt): 
-    user.pop('salt')   
+
+def generate_token(user, salt):
+    user.pop('salt')
     token = jwt.encode(user, salt, algorithm='HS256')
     return token
+
 
 def authenticate_user(client_id, token):
     user = get_user(client_id)
@@ -76,8 +89,9 @@ def authenticate_user(client_id, token):
             return 200
     return AUTH_FAILED, 401
 
+
 class registration(Resource):
-    def post(self): 
+    def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument("username")
         parser.add_argument("email")
@@ -90,17 +104,17 @@ class registration(Resource):
                 return EMAIL_EXISTS, 400
             if user['username'] == args['username']:
                 return UNAME_EXISTS, 400
-        
+
         user = {
-            "username":args['username'],
-            "email":args['email'],
+            "username": args['username'],
+            "email": args['email'],
             "password": pwd_context.hash(args['password']),
-            "id":get_user_id(),
-            "time_created":int(time.time())
+            "id": get_user_id(),
+            "time_created": int(time.time())
         }
-        
+
         salt = random_salt()
-        
+
         user.update(
             {
                 "salt": salt
@@ -117,6 +131,7 @@ class registration(Resource):
         )
         user.pop('password')
         return user, 201
+
 
 class acmang(Resource):
     def get(self, client_id):
@@ -147,7 +162,7 @@ class acmang(Resource):
         auth = authenticate_user(client_id, request_token)
         if auth != 200:
             return auth
-            
+
         parser = reqparse.RequestParser()
         parser.add_argument("username")
         parser.add_argument("email")
@@ -157,8 +172,8 @@ class acmang(Resource):
         updates = {}
         for key in args:
             if args[key] != None:
-                updates.update({key:args[key]})
-                
+                updates.update({key: args[key]})
+
         user = get_user(client_id)
         if user == None:
             return NOT_FOUND, 404
@@ -166,18 +181,19 @@ class acmang(Resource):
         json.dump(
             user,
             open(f"bin/{client_id}/account.json", "w"),
-            indent = 4
+            indent=4
         )
         user.pop('salt')
         user.pop('password')
         return user, 200
 
+
 class login(Resource):
     def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument("username")
-        parser.add_argument("password")      
-        args = parser.parse_args()  
+        parser.add_argument("password")
+        args = parser.parse_args()
 
         users = get_users()
         for user in users:
@@ -191,7 +207,8 @@ class login(Resource):
                     return user, 200
                 else:
                     return AUTH_FAILED, 401
-        return NOT_FOUND, 404 
+        return NOT_FOUND, 404
+
 
 class verify(Resource):
     def get(self, client_id):
