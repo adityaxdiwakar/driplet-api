8f166aceb94ed7ef2aaf7d589902455fbd6a8afa
Aditya Diwakar <i@adityadiwakar.me>
2019-03-01 19:51:07 -0500

    [fe] add option to delete service

--- backend/app.py
+++ backend/app.py
@@ -17,6 +17,7 @@ api = Api(app)
 def apply_caching(response):
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type, authorization'
     response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = '*'
     return response
 
 #writing services endpoints
