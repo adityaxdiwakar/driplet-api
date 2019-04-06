e14e76ecd5a33f5255f5f9f890af191bdbebad0e
Aditya Diwakar <i@adityadiwakar.me>
2019-04-06 14:41:27 -0400

    typo fixup

--- lib/accounts/utils.py
+++ lib/accounts/utils.py
@@ -37,14 +37,6 @@ def template(email, link):
               font-family: &#x27;Postmates Std&#x27;;
               font-weight: 600;
               font-style: normal;
-              src: local(&#x27;Postmates Std Bold&#x27;<title>Driplet Reset</title>
-<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
-<meta content="width=device-width" name="viewport">
-<style type="text/css">
-            @font-face {
-              font-family: &#x27;Postmates Std&#x27;;
-              font-weight: 600;
-              font-style: normal;
               src: local(&#x27;Postmates Std Bold&#x27;), url(https://s3-us-west-1.amazonaws.com/buyer-static.postmates.com/assets/email/postmates-std-bold.woff) format(&#x27;woff&#x27;);
             }
 
@@ -90,71 +82,7 @@ def template(email, link):
 </td>
 </tr>
 <tr>
-<td colspan="2" style="padding-top: 72px; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: 100%; color: #000000; font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; font-size: 48px; font-smoothing: always; font-style: normal; font-weight: 600; letter-spacing: -2.6px; line-height: 52px; mso-line-height-rule: exactly; text-decoration: none;">Reset your password</td>
-</tr>
-<tr>
-<td style="padding-top: 48px; padding-bottom: 48px;">
-<table cellpadding="0" cellspacing="0" style="width: 100%">
-<tbody><tr>
-<td style="width: 100%; height: 1px; max-height: 1px; background-color: #d9dbe0; opacity: 0.81"></td>
-</tr>
-</tbody></table>
-</td>
-</tr>
-<tr>
-<td style="-ms-text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: 100%; color: #9095a2; font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; font-size: 16px; font-smoothing: always; font-style: normal; font-weight: 400; letter-spacing: -0.18px; line-height: 24px; mso-line-height-rule: exactly; text-decoration: none; vertical-align: top; width: 100%;">
-                                      You're receiving this e-mail because you requested a password reset for your Driplet account.
-                                    </td>
-</tr>
-<tr>
-<td style="padding-top: 24px; -ms-text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: 100%; color: #9095a2; font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; font-size: 16px; font-smoothing: always; font-style: normal; font-weight: 400; letter-spacing: -0.18px; line-height: 24px; mso-line-height-rule: exactly; text-decoration: none; vertical-align: top; width: 100%;">
-                                      Please tap the button below to choose a new password.
-us-west-1.amazonaws.com/buyer-static.postmates.com/assets/email/postmates-std-bold.woff) format(&#x27;woff&#x27;);
-            }
-
-            @font-face {
-              font-family: &#x27;Postmates Std&#x27;;
-              font-weight: 500;
-              font-style: normal;
-              src: local(&#x27;Postmates Std Medium&#x27;), url(https://s3-us-west-1.amazonaws.com/buyer-static.postmates.com/assets/email/postmates-std-medium.woff) format(&#x27;woff&#x27;);
-            }
-
-            @font-face {
-              font-family: &#x27;Postmates Std&#x27;;
-              font-weight: 400;
-              font-style: normal;
-              src: local(&#x27;Postmates Std Regular&#x27;), url(https://s3-us-west-1.amazonaws.com/buyer-static.postmates.com/assets/email/postmates-std-regular.woff) format(&#x27;woff&#x27;);
-            }
-        </style>
-<style media="screen and (max-width: 680px)">
-            @media screen and (max-width: 680px) {
-                .page-center {
-                  padding-left: 0 !important;
-                  padding-right: 0 !important;
-                }
-                
-                .footer-center {
-                  padding-left: 20px !important;
-                  padding-right: 20px !important;
-                }
-            }
-        </style>
-</head>
-<body style="background-color: #f4f4f5;">
-<table cellpadding="0" cellspacing="0" style="width: 100%; height: 100%; background-color: #f4f4f5; text-align: center;">
-<tbody><tr>
-<td style="text-align: center;">
-<table align="center" cellpadding="0" cellspacing="0" id="body" style="background-color: #fff; width: 100%; max-width: 680px; height: 100%;">
-<tbody><tr>
-<td>
-<table align="center" cellpadding="0" cellspacing="0" class="page-center" style="text-align: left; padding-bottom: 88px; width: 100%; padding-left: 120px; padding-right: 120px;">
-<tbody><tr>
-<td style="padding-top: 24px;">
-<img src="https://driplet.cf/brand.png" style="width:120px; filter: invert(100%);">
-</td>
-</tr>
-<tr>
-<td colspan="2" style="padding-top: 72px; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: 100%; color: #000000; font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; font-size: 48px; font-smoothing: always; font-style: normal; font-weight: 600; letter-spacing: -2.6px; line-height: 52px; mso-line-height-rule: exactly; text-decoration: none;">Reset your password</td>
+<td colspan="2" style="padding-top: 72px; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: 100%; color: #000000; font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; font-size: 44px; font-smoothing: always; font-style: normal; font-weight: 600; letter-spacing: -2.6px; line-height: 52px; mso-line-height-rule: exactly; text-decoration: none;">Driplet password reset</td>
 </tr>
 <tr>
 <td style="padding-top: 48px; padding-bottom: 48px;">
