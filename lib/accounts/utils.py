7ec02a49ff864f910e89bb07fb903d61d80faefb
Aditya Diwakar <i@adityadiwakar.me>
2019-04-06 14:27:53 -0400

    testing email config

--- lib/accounts/utils.py
+++ lib/accounts/utils.py
@@ -1,9 +1,13 @@
+import string
+import time
+import jwt
 import random
 import json
 import os
 import pymongo
 import utils
 import requests as r
+
 
 def template(email, link):
     return {
@@ -23,15 +27,192 @@ def template(email, link):
         "content": [
             {
                 "type": "text/html",
-                "value": "<h1>Password reset</h1><br><p>" + link + "</p>"
+                "value": """
+                    <head>
+<title>Driplet Reset</title>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+<meta content="width=device-width" name="viewport">
+<style type="text/css">
+            @font-face {
+              font-family: &#x27;Postmates Std&#x27;;
+              font-weight: 600;
+              font-style: normal;
+              src: local(&#x27;Postmates Std Bold&#x27;<title>Driplet Reset</title>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+<meta content="width=device-width" name="viewport">
+<style type="text/css">
+            @font-face {
+              font-family: &#x27;Postmates Std&#x27;;
+              font-weight: 600;
+              font-style: normal;
+              src: local(&#x27;Postmates Std Bold&#x27;), url(https://s3-us-west-1.amazonaws.com/buyer-static.postmates.com/assets/email/postmates-std-bold.woff) format(&#x27;woff&#x27;);
+            }
+
+            @font-face {
+              font-family: &#x27;Postmates Std&#x27;;
+              font-weight: 500;
+              font-style: normal;
+              src: local(&#x27;Postmates Std Medium&#x27;), url(https://s3-us-west-1.amazonaws.com/buyer-static.postmates.com/assets/email/postmates-std-medium.woff) format(&#x27;woff&#x27;);
+            }
+
+            @font-face {
+              font-family: &#x27;Postmates Std&#x27;;
+              font-weight: 400;
+              font-style: normal;
+              src: local(&#x27;Postmates Std Regular&#x27;), url(https://s3-us-west-1.amazonaws.com/buyer-static.postmates.com/assets/email/postmates-std-regular.woff) format(&#x27;woff&#x27;);
+            }
+        </style>
+<style media="screen and (max-width: 680px)">
+            @media screen and (max-width: 680px) {
+                .page-center {
+                  padding-left: 0 !important;
+                  padding-right: 0 !important;
+                }
+                
+                .footer-center {
+                  padding-left: 20px !important;
+                  padding-right: 20px !important;
+                }
+            }
+        </style>
+</head>
+<body style="background-color: #f4f4f5;">
+<table cellpadding="0" cellspacing="0" style="width: 100%; height: 100%; background-color: #f4f4f5; text-align: center;">
+<tbody><tr>
+<td style="text-align: center;">
+<table align="center" cellpadding="0" cellspacing="0" id="body" style="background-color: #fff; width: 100%; max-width: 680px; height: 100%;">
+<tbody><tr>
+<td>
+<table align="center" cellpadding="0" cellspacing="0" class="page-center" style="text-align: left; padding-bottom: 88px; width: 100%; padding-left: 120px; padding-right: 120px;">
+<tbody><tr>
+<td style="padding-top: 24px;">
+<img src="https://driplet.cf/brand.png" style="width:120px; filter: invert(100%);">
+</td>
+</tr>
+<tr>
+<td colspan="2" style="padding-top: 72px; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: 100%; color: #000000; font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; font-size: 48px; font-smoothing: always; font-style: normal; font-weight: 600; letter-spacing: -2.6px; line-height: 52px; mso-line-height-rule: exactly; text-decoration: none;">Reset your password</td>
+</tr>
+<tr>
+<td style="padding-top: 48px; padding-bottom: 48px;">
+<table cellpadding="0" cellspacing="0" style="width: 100%">
+<tbody><tr>
+<td style="width: 100%; height: 1px; max-height: 1px; background-color: #d9dbe0; opacity: 0.81"></td>
+</tr>
+</tbody></table>
+</td>
+</tr>
+<tr>
+<td style="-ms-text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: 100%; color: #9095a2; font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; font-size: 16px; font-smoothing: always; font-style: normal; font-weight: 400; letter-spacing: -0.18px; line-height: 24px; mso-line-height-rule: exactly; text-decoration: none; vertical-align: top; width: 100%;">
+                                      You're receiving this e-mail because you requested a password reset for your Driplet account.
+                                    </td>
+</tr>
+<tr>
+<td style="padding-top: 24px; -ms-text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: 100%; color: #9095a2; font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; font-size: 16px; font-smoothing: always; font-style: normal; font-weight: 400; letter-spacing: -0.18px; line-height: 24px; mso-line-height-rule: exactly; text-decoration: none; vertical-align: top; width: 100%;">
+                                      Please tap the button below to choose a new password.
+us-west-1.amazonaws.com/buyer-static.postmates.com/assets/email/postmates-std-bold.woff) format(&#x27;woff&#x27;);
+            }
+
+            @font-face {
+              font-family: &#x27;Postmates Std&#x27;;
+              font-weight: 500;
+              font-style: normal;
+              src: local(&#x27;Postmates Std Medium&#x27;), url(https://s3-us-west-1.amazonaws.com/buyer-static.postmates.com/assets/email/postmates-std-medium.woff) format(&#x27;woff&#x27;);
+            }
+
+            @font-face {
+              font-family: &#x27;Postmates Std&#x27;;
+              font-weight: 400;
+              font-style: normal;
+              src: local(&#x27;Postmates Std Regular&#x27;), url(https://s3-us-west-1.amazonaws.com/buyer-static.postmates.com/assets/email/postmates-std-regular.woff) format(&#x27;woff&#x27;);
+            }
+        </style>
+<style media="screen and (max-width: 680px)">
+            @media screen and (max-width: 680px) {
+                .page-center {
+                  padding-left: 0 !important;
+                  padding-right: 0 !important;
+                }
+                
+                .footer-center {
+                  padding-left: 20px !important;
+                  padding-right: 20px !important;
+                }
+            }
+        </style>
+</head>
+<body style="background-color: #f4f4f5;">
+<table cellpadding="0" cellspacing="0" style="width: 100%; height: 100%; background-color: #f4f4f5; text-align: center;">
+<tbody><tr>
+<td style="text-align: center;">
+<table align="center" cellpadding="0" cellspacing="0" id="body" style="background-color: #fff; width: 100%; max-width: 680px; height: 100%;">
+<tbody><tr>
+<td>
+<table align="center" cellpadding="0" cellspacing="0" class="page-center" style="text-align: left; padding-bottom: 88px; width: 100%; padding-left: 120px; padding-right: 120px;">
+<tbody><tr>
+<td style="padding-top: 24px;">
+<img src="https://driplet.cf/brand.png" style="width:120px; filter: invert(100%);">
+</td>
+</tr>
+<tr>
+<td colspan="2" style="padding-top: 72px; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: 100%; color: #000000; font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; font-size: 48px; font-smoothing: always; font-style: normal; font-weight: 600; letter-spacing: -2.6px; line-height: 52px; mso-line-height-rule: exactly; text-decoration: none;">Reset your password</td>
+</tr>
+<tr>
+<td style="padding-top: 48px; padding-bottom: 48px;">
+<table cellpadding="0" cellspacing="0" style="width: 100%">
+<tbody><tr>
+<td style="width: 100%; height: 1px; max-height: 1px; background-color: #d9dbe0; opacity: 0.81"></td>
+</tr>
+</tbody></table>
+</td>
+</tr>
+<tr>
+<td style="-ms-text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: 100%; color: #9095a2; font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; font-size: 16px; font-smoothing: always; font-style: normal; font-weight: 400; letter-spacing: -0.18px; line-height: 24px; mso-line-height-rule: exactly; text-decoration: none; vertical-align: top; width: 100%;">
+                                      You're receiving this e-mail because you requested a password reset for your Driplet account.
+                                    </td>
+</tr>
+<tr>
+<td style="padding-top: 24px; -ms-text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: 100%; color: #9095a2; font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; font-size: 16px; font-smoothing: always; font-style: normal; font-weight: 400; letter-spacing: -0.18px; line-height: 24px; mso-line-height-rule: exactly; text-decoration: none; vertical-align: top; width: 100%;">
+                                      Please tap the button below to choose a new password.
+                                    </td>
+</tr>
+<tr>
+<td>
+<a data-click-track-id="37" href=""" + link + """ style="margin-top: 36px; -ms-text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: 100%; color: #ffffff; font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; font-size: 12px; font-smoothing: always; font-style: normal; font-weight: 600; letter-spacing: 0.7px; line-height: 48px; mso-line-height-rule: exactly; text-decoration: none; vertical-align: top; width: 220px; background-color: #1a1a1a; border-radius: 28px; display: block; text-align: center; text-transform: uppercase" target="_blank">
+                                        Reset Password
+                                      </a>
+</td>
+</tr>
+</tbody></table>
+</td>
+</tr>
+</tbody></table>
+<table align="center" cellpadding="0" cellspacing="0" id="footer" style="background-color: #1a1a1a; width: 100%; max-width: 680px; height: 100%;">
+<tbody><tr>
+<td>
+<table align="center" cellpadding="0" cellspacing="0" class="footer-center" style="text-align: left; width: 100%; padding-left: 120px; padding-right: 120px;">
+<tbody><tr>
+</tr>
+<tr>
+<td colspan="2" style="padding-top: 24px; padding-bottom: 48px;">
+<table cellpadding="0" cellspacing="0" style="width: 100%">
+<tbody><tr>
+</tr>
+</tbody></table>
+</td>
+</tr>
+</tbody></table>
+</td>
+</tr>
+</tbody></table>
+
+
+
+</body>
+                """
             }
         ]
     }
 
-import string
-import random
-import jwt
-import time
 
 def password_reset(identification):
     x = utils.col.find({"username": identification})
@@ -44,26 +225,30 @@ def password_reset(identification):
         user = y
     elif z.count() > 0:
         user = z
-    
-    if user == None:
-        return 404 #not found
 
-    user = utils.encoder(user)[0] #get the single user out of the array it's returned in
+    if user == None:
+        return 404  # not found
+
+    # get the single user out of the array it's returned in
+    user = utils.encoder(user)[0]
     email = user["email"]
     userid = user["id"]
-    encoded_jwt = jwt.encode({'expiration': int(time.time()) + 3600, 'user': userid}, user["salt"], algorithm='HS256')
+    encoded_jwt = jwt.encode({'expiration': int(
+        time.time()) + 600, 'user': userid}, user["salt"], algorithm='HS256')
     encoded_jwt = encoded_jwt.decode('utf-8')
-    e_tem = template(email, f"https://driplet.cf/reset?={encoded_jwt}&user={userid}")
+    e_tem = template(
+        email, f"https://driplet.cf/reset?={encoded_jwt}&user={userid}")
     s = r.post(
         "https://api.sendgrid.com/v3/mail/send",
-        json = e_tem,
-        headers = {
+        json=e_tem,
+        headers={
             "Authorization": "Bearer " + os.getenv("API_KEY"),
             "Content-Type": "application/json"
         }
     )
     if s.status_code != 202:
         return 400
+
 
 def get_user_id():
     while True:
