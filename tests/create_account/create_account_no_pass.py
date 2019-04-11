32c3395da0a6439e750b79e448d88c51bde6b021
Aditya Diwakar <i@adityadiwakar.me>
2019-04-11 19:28:27 -0400

    Creating account on expected 401 deletes itself

--- tests/create_account/create_account_no_pass.py
+++ tests/create_account/create_account_no_pass.py
@@ -8,6 +8,9 @@ x = a_api.make_account({
 
 #checking the server response
 if x.status_code != 400:
+    #deleting account so the other tests don't fail
+    user = x.json()
+    a_api.delete_account(user["id"], user["token"])
     raise Exception(
         f"Account creation w/ no password did not return 400 response, instead returned {x.status_code}"
     )
